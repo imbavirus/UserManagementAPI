8a898fdd2cb22aa68b2e27158481300ff4b37a44
Justin van der Westhuizen <justin@giftsinc.co.za>
2025-05-10 00:29:36 +0200

    Added swagger

--- Program.cs
+++ Program.cs
@@ -2,15 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(); 
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    // You can customize the Swagger UI endpoint here if needed
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "User Profile API V1");
+});
 
 app.UseHttpsRedirection();
 
